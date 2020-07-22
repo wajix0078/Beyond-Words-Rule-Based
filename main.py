1458b3c76bd446d6c68ba06556c906a780c15c66
wajix0078 <wajahatmasood886@gmail.com>
2020-07-23 01:11:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ import SentenceGenerator
 def Print():
     m =  SentenceGenerator.Sentence_Generator()
     return(m.generate_sentence(("am","9","44","work"),sentence_type='declaration',tense='present',s_dtnum=('I am',1),o1_dtnum=('',1),o2_dtnum=('',1),o3_dtnum=('',1)))
-print("I love U",Print())    
+print(Print())    
